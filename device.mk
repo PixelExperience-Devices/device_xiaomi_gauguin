a7ea9fa4a5ecbcf7d0974db29c336110d580575a
Bruno Martins <bgcngm@gmail.com>
2020-02-15 23:18:43 +0000

    gauguin: Enable device-specific features
    Change-Id: I63661e12d78c82e524fc1f8240b3494de8f86d13
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- device.mk
+++ device.mk
@@ -44,7 +44,10 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.ims.xml
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.ims.xml \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.consumerir.xml \
+    frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.wifi.aware.xml \
+    frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.wifi.rtt.xml
 
 # Telephony
 PRODUCT_PACKAGES += \
