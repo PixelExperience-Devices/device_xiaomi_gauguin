b7c43b9e927c328414810df633172b1a58fa88fc
Peter Cai <peter@typeblog.net>
2020-02-29 15:41:22 +0800

    gauguin: build our own Light HAL
    * The Light HAL in official vendor partition does not work with AOSP.
      Let's get rid of it and build our own.
    * Use vendor_overlay to override the original one.
    
    Co-Authored-By: LuK1337 <priv.luk@gmail.com>
    Co-Authored-By: Demon000 <demonsingur@gmail.com>
    Co-Authored-By: Michael Bestas <mkbestas@lineageos.org>
    Co-Authored-By: Wang Han <416810799@qq.com>

--- device.mk
+++ device.mk
@@ -47,6 +47,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.xiaomi_gauguin
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
