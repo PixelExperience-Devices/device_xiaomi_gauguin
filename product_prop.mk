3e1f7c475d440e8c908f9f922f7e9d889fd620df
Peter Cai <peter@typeblog.net>
2020-03-02 16:49:27 +0800

    gauguin: Override ro.adb.secure for eng builds
    * `/vendor/default.prop` is setting `ro.adb.secure=1`.

--- product_prop.mk
+++ product_prop.mk
@@ -4,6 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Adb
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+# /vendor/default.prop is force-setting ro.adb.secure=1
+# Get rid of that by overriding it in /product on eng builds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.secure=0 \
+    ro.adb.secure=0
+endif
+
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
     vendor.camera.aux.packagelist=org.lineageos.snap,net.sourceforge.opencamera,org.codeaurora.snapcam
