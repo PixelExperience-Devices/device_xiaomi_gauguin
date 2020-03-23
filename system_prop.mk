d1b93749c29639d26c53c95d2ad77492bb1e70c3
Lucas Dupin <dupin@google.com>
2020-03-23 10:55:10 -0700

    gauguin: Default blurs to on on coral and flame
    Bug: 149792636
    Test: make
    Change-Id: Id78b72bd3925db21c4c963298c3684e3b54ba43a

--- system_prop.mk
+++ system_prop.mk
@@ -18,10 +18,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=440
 
-# Enable blurs, hidden under dev option
+# Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
-    persist.sys.sf.disable_blurs=1 \
     ro.sf.blurs_are_expensive=1  
 
 # Netflix
