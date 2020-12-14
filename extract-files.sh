392d00739e32e0f662720f5aa843864aabb14713
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2020-12-14 13:32:45 +0100

    gauguin: Correct XML version in QTI data connection permission XML
    Change-Id: I0bba9ecf6976bf1101e90a3bb835c08409ac4fc2

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
+            sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
+            ;;
     esac
 }
 
