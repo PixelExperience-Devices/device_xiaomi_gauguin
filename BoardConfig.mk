8e50b8fc785cccfe62bcfe10dcea94b3d64cb439
AdarshGrewal <adarshgrewal@gmail.com>
2021-01-20 20:41:35 +0530

    gauguin: include private sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,6 +101,9 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
+# Sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR := $(DEVICE_PATH)/sepolicy/private
+
 # Telephony
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
