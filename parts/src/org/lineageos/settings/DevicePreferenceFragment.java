53a32715f330a2650a4b266542f20a722567e0e0
Akhil Narang <me@akhilnarang.dev>
2021-01-26 11:49:44 +0530

    gauguin: parts: fix refresh rate switching
      * Added in a 90hz option - used SurfaceFlinger for this
        (All credits to DD3Boh for the `RefreshRateUtils.setFPS()` method)
    
      * Thanks to Abhishek for helping with fixing the BootCompletedReceiver
    
      * Also formatted code in android studio
    
      * Thanks xyyx for fixing refresh rate values
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    Signed-off-by: AdarshGrewal <adarshgrewal@gmail.com>

--- parts/src/org/lineageos/settings/DevicePreferenceFragment.java
+++ parts/src/org/lineageos/settings/DevicePreferenceFragment.java
@@ -17,10 +17,12 @@
 package org.lineageos.settings;
 
 import android.os.Bundle;
-import android.provider.Settings;
-import androidx.preference.PreferenceFragment;
-import androidx.preference.Preference;
+
 import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+
+import org.lineageos.settings.utils.RefreshRateUtils;
 
 public class DevicePreferenceFragment extends PreferenceFragment {
     private static final String KEY_MIN_REFRESH_RATE = "pref_min_refresh_rate";
@@ -38,30 +40,28 @@ public class DevicePreferenceFragment extends PreferenceFragment {
         addPreferencesFromResource(R.xml.device_prefs);
         mPrefMinRefreshRate = (ListPreference) findPreference(KEY_MIN_REFRESH_RATE);
         mPrefMinRefreshRate.setOnPreferenceChangeListener(PrefListener);
-        updateValuesAndSummaries();
     }
 
-    private void updateValuesAndSummaries() {
-        final float refreshRate = Settings.System.getFloat(getContext().getContentResolver(),
-            Settings.System.MIN_REFRESH_RATE, 120.0f);
-        mPrefMinRefreshRate.setValue(((int) refreshRate) + " Hz");
-        mPrefMinRefreshRate.setSummary(mPrefMinRefreshRate.getValue());
+    @Override
+    public void onResume() {
+        super.onResume();
+        mPrefMinRefreshRate.setValue(Integer.toString(RefreshRateUtils.getRefreshRate(getActivity())));
+        mPrefMinRefreshRate.setSummary(mPrefMinRefreshRate.getEntry());
     }
 
-    private Preference.OnPreferenceChangeListener PrefListener =
-        new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object value) {
-                final String key = preference.getKey();
+    private final Preference.OnPreferenceChangeListener PrefListener =
+            new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object value) {
+                    final String key = preference.getKey();
 
-                if (KEY_MIN_REFRESH_RATE.equals(key)) {
-                    Settings.System.putFloat(getContext().getContentResolver(),
-                        Settings.System.MIN_REFRESH_RATE,
-                        (float) Integer.parseInt((String) value));
+                    if (KEY_MIN_REFRESH_RATE.equals(key)) {
+                        RefreshRateUtils.setRefreshRate(getActivity(), Integer.parseInt((String) value));
+                        RefreshRateUtils.setFPS(Integer.parseInt((String) value));
+                        int MinRefreshRateIndex = mPrefMinRefreshRate.findIndexOfValue((String) value);
+                        mPrefMinRefreshRate.setSummary(mPrefMinRefreshRate.getEntries()[MinRefreshRateIndex]);
+                    }
+                    return true;
                 }
-
-                updateValuesAndSummaries();
-                return true;
-            }
-        };
+            };
 }
