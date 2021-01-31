6cab41b8c36f9ac350a33ff503d9525951e9475c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-31 16:46:03 +0530

    gauguin: parts: Add ACTION_SCREEN_ON intent filter
     * this should fix thermal/touchscreen profiles not
       getting restored after turning screen back on
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- parts/src/org/lineageos/settings/thermal/ThermalService.java
+++ parts/src/org/lineageos/settings/thermal/ThermalService.java
@@ -80,6 +80,7 @@ public class ThermalService extends Service {
     private void registerReceiver() {
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_SCREEN_ON);
         this.registerReceiver(mIntentReceiver, filter);
     }
 
