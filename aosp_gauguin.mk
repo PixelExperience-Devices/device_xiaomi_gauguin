73a471f2fdfb0283d01f8734a7873bc624bab041
baolong24 <1667375308@qq.com>
2021-03-16 17:22:34 +0800

    gauguin: Switch to common_full_phone common config

--- aosp_gauguin.mk
+++ aosp_gauguin.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, device/xiaomi/gauguin/device.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
