1d766d205b18f8d423d690ebf266b9356861e03c
baolong24 <1667375308@qq.com>
2021-04-03 18:35:11 +0800

    gauguin: Build ANT

--- device.mk
+++ device.mk
@@ -30,6 +30,10 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService-Soong
+
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
