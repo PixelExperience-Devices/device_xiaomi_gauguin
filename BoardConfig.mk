ceb64319796692bdea120890eee074c427d1330d
jjhitel <jjjhitel@gmail.com>
2021-04-10 19:57:53 +0800

    gauguin: Kang sepolicy rules from picasso
    Signed-off-by: baolong24 <1667375308@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,6 +137,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 # Sepolicy
 include device/qcom/sepolicy/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Telephony
