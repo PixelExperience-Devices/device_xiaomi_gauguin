402924ea94a32cb846cbcd1acecfd7ad952dcb30
EcrosoftXiao <xy19990903@gmail.com>
2021-04-20 22:04:52 +0800

    gauguin: parts: move to common custom resources
    Signed-off-by: baolong24 <1667375308@qq.com>

--- parts/Android.mk
+++ parts/Android.mk
@@ -22,7 +22,8 @@ LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
-    androidx.preference_preference
+    androidx.preference_preference \
+    org.pixelexperience.settings.resources
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
