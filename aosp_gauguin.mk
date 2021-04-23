32086c0900aee2382d1bd9ac2379aa009474d062
baolong24 <1667375308@qq.com>
2021-04-23 19:47:29 +0800

    gauguin: Use AOSP Recovery

--- aosp_gauguin.mk
+++ aosp_gauguin.mk
@@ -9,6 +9,7 @@ $(call inherit-product, device/xiaomi/gauguin/device.mk)
 
 # Inherit some common PixelExperience stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
