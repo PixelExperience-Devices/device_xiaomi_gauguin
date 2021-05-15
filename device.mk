c42a2b000aa98cfd5d5d93fd61237176c20ad93d
AdarshGrewal <adarsh@aosip.dev>
2021-05-15 16:34:34 +0530

    gauguin: drop aosp dalvik heep configs

--- device.mk
+++ device.mk
@@ -8,9 +8,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
