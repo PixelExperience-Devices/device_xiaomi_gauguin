5ac57bccbad534a3a0b6bde240068cc0e548fb07
baolong24 <1667375308@qq.com>
2021-07-23 22:01:46 +0800

    gauguin: Drop unused flags

--- aosp_gauguin.mk
+++ aosp_gauguin.mk
@@ -14,7 +14,6 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_gauguin
