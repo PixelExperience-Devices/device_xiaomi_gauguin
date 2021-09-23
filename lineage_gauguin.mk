9d526a4a2abdcb9eb47672f8aefb5e194cabef1c
baolong24 <1667375308@qq.com>
2021-09-24 01:01:56 +0800

    gauguin: Initial skeleton tree
    Signed-off-by: baolong24 <1667375308@qq.com>
    Change-Id: I02a9ce467695c8d75a81322c2dba1692bb7e4cc4

---
+++ lineage_gauguin.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from gauguin device
+$(call inherit-product, device/xiaomi/gauguin/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_gauguin
+PRODUCT_DEVICE := gauguin
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := M2007J17G
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := Xiaomi/gauguin_global/gauguin:11/RKQ1.200826.002/V12.5.2.0.RJSMIXM:user/release-keys
