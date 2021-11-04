970442590561ee965717091b538ea9f4db13c8f2
baolong24 <1667375308@qq.com>
2021-11-04 17:12:49 +0800

    gauguin: Update vendor security patch
    Signed-off-by: baolong24 <1667375308@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,7 +151,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-06-01
+VENDOR_SECURITY_PATCH := 2021-09-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
