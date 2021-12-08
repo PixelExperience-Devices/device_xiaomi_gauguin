3eb5e2ca447cedfcfe8c54a051ef06f669d85aef
baolong24 <1667375308@qq.com>
2021-12-08 15:28:52 +0800

    gauguin: Add missing FM blobs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,6 +74,10 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
+# FM
+BOARD_HAS_QCA_FM_SOC := "cherokee"
+BOARD_HAVE_QCOM_FM := true
+
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
