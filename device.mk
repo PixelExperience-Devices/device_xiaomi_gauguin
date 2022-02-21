b17391b317497e3f34c966169eb3698c7ca8d79e
LynnrinChan <wuxiaofei31@gmail.com>
2022-02-21 16:00:45 +0800

    Revert "gauguin: Compile libldacBT_bco.so"
    This reverts commit d9580de50c7d9cce0564069d97cc22c64d7f9f3f.

--- device.mk
+++ device.mk
@@ -147,8 +147,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor \
-    libldacBT_bco
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
